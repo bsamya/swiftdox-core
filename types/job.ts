fa22d2773ac9b0bf61474a28b3e8c2b26dbb9d7d
bsamya <bsamya@gmail.com>
2023-12-04 11:45:31 -0500

    init

---
+++ types/job.ts
@@ -0,0 +1,94 @@
+import { Timestamp } from '@firebase/firestore';
+
+type Result = {
+  approved?: boolean;
+  declined?: string[]
+}
+
+export interface JobDocument {
+  id: string;
+  documentType: null | 'legal' | 'shipping';
+  approved?: boolean;
+  declined?: false | string[];
+}
+
+export interface LegalDocument {
+  id: string;
+  result: undefined | Result;
+  approved?: boolean;
+  declined?: false | string[];
+  documentType: 'legal';
+  documentTypeId: string | null
+  attachment: string | null
+}
+
+export interface ShippingDocument {
+  id: string;
+  result: undefined | Result;
+  documentType: 'shipping';
+  documents: { [key: string]: number };
+  attachments: string[];
+}
+
+export enum JobStatus {
+  draft = 'draft',
+  pending = 'pending',
+  submitted = 'submitted',
+  completed = 'completed',
+  withdrawn = 'withdrawn',
+}
+
+export enum JobEvent {
+  created = 'created',
+  lastUpdated = 'lastUpdated',
+  submitted = 'submitted',
+  completed = 'completed',
+  withdrawn = 'withdrawn',
+}
+
+type JobSummary = {
+  pending: number,
+  approved: number,
+  declined: number,
+}
+
+export type Job = {
+  jobId: string;
+  profileId: string;
+  agentId: string | null;
+  reference: string;
+  country: string;
+  status: JobStatus
+  items: (ShippingDocument | LegalDocument)[];
+  events: { [key in JobEvent]?: { date: Timestamp, by: string } };
+
+  summary: {
+    shippingDocuments: JobSummary
+    legalDocuments: JobSummary
+  },
+
+  billing?: {
+    preAuthAmount: number,
+    transactionId: string,
+  }
+  invoice?: {
+    qty: number,
+    fee: number,
+    total: number
+  }
+}
+
+export interface invoice {
+  id: string;
+  date: Timestamp;
+  profileId: string;
+  documentCount: number;
+  costPerDocument: number;
+  amount: number;
+}
+
+export interface InvoiceItems {
+  description: string;
+  quantity: number;
+  cost: number;
+}
