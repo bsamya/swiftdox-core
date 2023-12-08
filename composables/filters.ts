01122ae15eb13a7ffc0611288ede7f1903870ef5
bsamya <bsamya@gmail.com>
2023-12-08 10:51:41 -0500

    filters

---
+++ composables/filters.ts
@@ -0,0 +1,6 @@
+export const jobFilters = [
+  { key: "pending", title: "Show Pending Jobs" },
+  { key: "completed", title: "Show Completed Jobs" },
+  { key: "draft", title: "Show Draft Jobs" },
+  { key: "withdrawn", title: "Show Withdrawn Jobs" },
+]
