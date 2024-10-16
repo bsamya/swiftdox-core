dc8bb78544ad1d836fc5dd1cffc8895b8abe0c41
bsamya <bsamya@gmail.com>
2024-10-16 09:52:21 -0400

    Update package.json dependencies and nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,26 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     "@pinia/nuxt",
     "@nuxtjs/tailwindcss",
     "vuetify-nuxt-module",
     "@nuxt/image",
   ],
+
   tailwindcss: {
     viewer: false
   },
 
   runtimeConfig: {
-    authorizeNet: JSON.parse(process.env.AUTHORIZE_NET),
-    firebase: JSON.parse(process.env.FIREBASE_SERVER),
-    mailer: JSON.parse(process.env.MAILER),
+    authorizeNet: JSON.parse(process.env.AUTHORIZE_NET as string),
+    firebase: JSON.parse(process.env.FIREBASE_SERVER as string),
+    mailer: JSON.parse(process.env.MAILER as string),
     public: {
       googleApiKey: process.env.GOOGLE_API_KEY,
-      firebase: JSON.parse(process.env.FIREBASE_CLIENT),
+      firebase: JSON.parse(process.env.FIREBASE_CLIENT as string),
     },
   },
 
+  compatibilityDate: "2024-12-31",
 })
